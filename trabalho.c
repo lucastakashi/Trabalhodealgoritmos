1230ebb2e0092c8569936ffd9e96b2b046d3154c
lucastakashi <48793578+lucastakashi@users.noreply.github.com>
2019-03-21 09:39:20 -0300

    Add files via upload

---
+++ trabalho.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+void main (){
+int num;
+printf ("digite um numero inteiro ");
+scanf ("%d", &num);
+if (num %3==0 || num %5==0){
+    printf("é divisivel por 3 ou 5. \n");
+    
+}
+
+
+ else
+
+{
+    printf("não é divisivel por 3 e 5. \n");
+}
+
+}
