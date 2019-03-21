4fa717d277934d3ff692a6783b1e538ca6b4b097
lucastakashi <48793578+lucastakashi@users.noreply.github.com>
2019-03-21 11:36:14 -0300

    Add files via upload

---
+++ trabalho2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void main (){
+int num1;
+int num2;
+int num3;
+int num4;
+int soma;
+printf ("digite quatro numeros inteiros \n");
+printf("digite o primeiro numero:..");
+scanf("%d", &num1);
+printf("digite o segundo numero:..");
+scanf("%d", &num2);
+printf("digite o terceiro numero:..");
+scanf("%d", &num3);
+printf("digite o quarto numero:..");
+scanf("%d", &num4);
+
+if ("(num1<num2) && (num1<3) && (num1>num4)"){
+    printf("%d + %d + %d = %d\n", num2,num3,num4,num2+num3+num4);
+   
+    
+                                          
+}
+
+}
